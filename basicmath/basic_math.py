d1d56ab1e45fb1f91e073725bb28a5037141a204
Joch <joch41@gmail.com>
2020-11-12 08:08:28 +0100

    TI

--- basicmath/basic_math.py
+++ basicmath/basic_math.py
@@ -1,5 +1,6 @@
 
 def calculator(operation, a, b):
+    store_operation_in_database(operation, a, b)
     if operation == '+':
         return sum(a, b)
     else:
@@ -9,3 +10,8 @@ def calculator(operation, a, b):
 def sum(a, b):
     return a + b
 
+
+def store_operation_in_database(operation, a, b):
+    f = open("logs.txt", "w+")
+    f.write(str(a) + " " + operation + " " + str(b))
+    f.close()
---
+++ test/integration/test_basic_math.py
@@ -0,0 +1,24 @@
+
+def test_sum():
+    import basicmath.basic_math as m
+    assert m.sum(1, 2) == 3
+    assert m.sum(2, 3) == 5
+    assert m.sum(-1, 1) == 0
+
+
+def test_calculator(monkeypatch):
+    import basicmath.basic_math as m
+    monkeypatch.setattr(m, 'sum', mocked_sum_with_fake_return)
+    assert m.calculator('+', 1 , 2) == "toto"
+    assert m.calculator('+', 3 , 5) == "toto"
+
+
+def test_calculator_ti():
+    import basicmath.basic_math as m
+    assert m.calculator('+', 1, 2) == 3
+    f = open("logs.txt","r+")
+    assert '1 + 2' in f.read()
+
+
+def mocked_sum_with_fake_return(a, b):
+    return "toto"
